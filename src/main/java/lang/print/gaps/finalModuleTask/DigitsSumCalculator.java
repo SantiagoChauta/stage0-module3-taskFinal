db0adcc365ea1aae32032937d0be0196aa1107ce
Santiago Chauta <santiagochauta@gmail.com>
2023-06-07 10:37:04 -0500

    finish

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+    	int last = number%10;
+    	number/=10;
+    	int third = number%10;
+    	number/=10;
+    	int second = number%10;
+    	number/=10;
+    	int first = number %10;
+    	int ans = first+second+third+last;
+    	System.out.println(ans);
     }
 }
